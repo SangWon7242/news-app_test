c88aab7924f87e799cad511b35377e945c6c3f1f
SangWon7242 <gotkdqja@gmail.com>
2024-05-09 15:29:11 +0900

    뉴스 API 적용

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <div>Hello</div>;
-}
+"use client";
+
+import React, { useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=4b9d1c190bc4462d9445df3d6cddc1ea"
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          cols={30}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+};
+export default App;
